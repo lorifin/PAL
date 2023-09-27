0b3618ddea5c5e5bead5d0967f4a41dd1da6e6d3
Stéphane Krebs <skre888@gmail.com>
2023-09-27 12:10:45 +0200

    Update page how it works

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   get 'pages/help'
-  root 'pages#home' 
   get  '/about' => 'pages#about'
   get '/help' => 'pages#help'
-  get '/help' => 'pages#help'
+  get '/home' => 'pages#home'
   get '/how-it-works' => 'pages#how_it_works'
 end
